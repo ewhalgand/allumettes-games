36e9fff63ca0eada0a795816076e77d1a5d99b08
ewhalgand <halgandewen44@gmail.com>
2025-03-04 21:20:46 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,63 @@
+const numberValue = document.querySelector("#number-value") as HTMLInputElement;
+const numberChoiceValue = document.querySelector("#number-choice-value") as HTMLInputElement;
+
+const numberMatches = document.querySelector("#number-matches") as HTMLParagraphElement;
+const allPlayers = document.querySelector("#player") as HTMLParagraphElement;
+
+const form = document.querySelector("#form") as HTMLFormElement;
+const formMulti = document.querySelector("#form-multi") as HTMLFormElement;
+const error = document.querySelector("#error") as HTMLParagraphElement;
+
+const setPlayer = document.querySelector("#set-player") as HTMLDivElement;
+const games = document.querySelector("#games") as HTMLDivElement;
+
+let multiGame: number;
+
+formMulti.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const multiPlayer = Number(numberChoiceValue.value);
+
+  multiGame = multiPlayer > 0 ? multiPlayer : 1;
+
+  numberMatchToRemove(10);
+});
+
+const changeDisplay = () => {
+  if (!multiGame) {
+    games.style.display = "none";
+  } else {
+    games.style.display = "block";
+    setPlayer.style.display = "none";
+  }
+};
+changeDisplay();
+
+const numberMatchToRemove = (numberToRemove: number) => {
+  const players = multiGame;
+  let currentPlayer = 1;
+  changeDisplay();
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let playerValue = Number(numberValue.value);
+
+    if (playerValue > 6 || playerValue < 1 || isNaN(playerValue)) {
+      error.textContent = "Le nomber n'est pas compris entre 1 - 6";
+      return;
+    }
+
+    numberMatches.textContent = `Nombre d'Allumettes restants ${(numberToRemove -=
+      playerValue)}`;
+    numberValue.value = "";
+
+    if (numberToRemove <= 0) {
+      numberMatches.textContent = `Joueur ${currentPlayer} a gagné !`;
+      return;
+    }
+
+    currentPlayer = (currentPlayer % players) + 1;
+    allPlayers.textContent = `Joueur ${currentPlayer} :`;
+  });
+};
