5c52749cca7d0ceba11f53342495f486299ff2fc
ewhalgand <halgandewen44@gmail.com>
2025-03-04 09:19:49 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const multiplayerGames = () => {
+  const multiPlayer = prompt("Nombre de joueur ?");
+
+  return multiPlayer > 0 ? multiPlayer : 1;
+};
+
+const numberMatchToRemove = (numberToRemove) => {
+    const players = multiplayerGames();
+    let currentPlayer = 1;
+
+  while (numberToRemove > 0) {
+    const player = Number(prompt(`Joueur ${currentPlayer} : Combien d'alumette soutez fou retirer ? (1 - 6)`))
+
+    console.log({ players: player });
+
+    if (player > 6 || player < 1 || isNaN(player)) {
+      alert("Le nomber n'est pas compris entre 1 - 6");
+     continue;
+    }
+
+    numberToRemove -= player
+    console.log(`Joueur ${currentPlayer} a retiré ${player} allumettes. Il reste ${numberToRemove} allumettes.`);
+
+    if (numberToRemove <= 0) {
+      alert(`Joueur ${currentPlayer} a gagné !`);
+      return;
+    }
+
+    currentPlayer = (currentPlayer % players) + 1;
+  }
+};
+numberMatchToRemove(50);
